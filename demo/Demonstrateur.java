337466a59de7df709b6eac63e255cd2a6d57f83b
Hadrien Eyraud <Hadrien.Eyraud@grenoble-inp.org>
2022-11-16 10:21:27 +0100

    feat: class squelett

--- demo/Demonstrateur.java
+++ demo/Demonstrateur.java
@@ -5,23 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Demonstrateur {
-    static Connection bd;
-    public static void main(String[] args) {
-        System.out.println("Bienvenue dans le démonstrateur du projet BD du groupe 5 (ISI1).");
-        try{
-            System.out.println("Importation du driver...");
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-            System.out.println("Connection à la base de données...");
-            bd = DriverManager.getConnection("jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1", "eyraudh", "eyraudh");
-            System.out.println("Connection réussie !");
-        }catch(SQLException e) {
-            e.printStackTrace();
-        }
-            System.out.println("ici on fait les dingz");
-        try{
-            bd.close();
-        }catch(SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	static Connection bd;
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenue dans le démonstrateur du projet BD du groupe 5 (ISI1).");
+		try {
+			System.out.println("Importation du driver...");
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			System.out.println("Connection à la base de données...");
+			bd = DriverManager.getConnection("jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1", "eyraudh", "eyraudh");
+			System.out.println("Connection réussie !");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ici on fait les dingz");
+		try {
+			bd.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
