5dbd6a0aca3be65261c461a9c847d1d378ff6991
Theo <theo.riaille@grenoble-inp.org>
2022-11-27 12:24:13 +0100

    added database creation and deletion

--- src/tables/Horaires.java
+++ src/tables/Horaires.java
@@ -1,6 +1,11 @@
 package tables;
 
 import demo.Console;
+import demo.Database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Horaires {
     private Jour jour;
@@ -9,6 +14,20 @@ public class Horaires {
 
     public static void parseList() {
         // TODO afficher les horaires contenues dans la bdd
+        try {
+            PreparedStatement stmt = Database.getDb().prepareStatement
+                    ("SELECT * FROM HORAIRE");
+            ResultSet rset = stmt.executeQuery();
+
+            while (rset.next()) {
+                for (int i = 0; i < 1; i++) {
+                    System.out.println(rset.getString(i));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL request failed");
+            e.printStackTrace(System.err);
+        }
     }
 
     public static void parseAdd() {
